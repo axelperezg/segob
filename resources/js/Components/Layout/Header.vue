fc75097d81b41e44d6d5c01850f0aec450c3fc6f
miguelbonifaz <miguelbonifaz126@gmail.com>
2025-03-05 20:09:58 -0500

    Add header background and update Header component styling

--- resources/js/Components/Layout/Header.vue
+++ resources/js/Components/Layout/Header.vue
@@ -5,28 +5,30 @@ const appSettings = usePage().props.app_settings
 </script>
 
 <template>
-    <div class="py-8 md:flex md:justify-between md:items-center mx-auto max-w-6xl">
-        <h1 class="flex flex-col text-4xl font-bold text-gold">
-            <Link href="/">
-            <figure>
-                <img class="w-[12rem]" :src="`/storage/${appSettings.logo}`" alt="Segob Noticias" />
-            </figure>
-            </Link>
-        </h1>
-        <div class="flex items-center">
-            <div class="hidden md:block pr-6">
-                <div class="flex items-center justify-center gap-4">
-                    <template v-for="social in appSettings.social_networks" :key="social.network">
-                        <a :href="social.url" target="_blank">
-                            <i
-                                :class="`text-3xl text-burgundy fa-brands fa-${social.network === 'twitter' ? 'x-twitter' : social.network}`"></i>
-                        </a>
-                    </template>
+    <div class="-mx-4 bg-cover bg-center" style="background-image: url('/assets/bg-header.jpg'); ">
+        <div class="px-4 py-8 mx-auto max-w-6xl md:flex md:justify-between md:items-center">
+            <h1 class="flex flex-col text-4xl font-bold text-gold">
+                <Link href="/">
+                <figure>
+                    <img class="w-[12rem]" :src="`/storage/${appSettings.logo}`" alt="Segob Noticias" />
+                </figure>
+                </Link>
+            </h1>
+            <div class="flex items-center">
+                <div class="hidden md:block pr-6">
+                    <div class="flex items-center justify-center gap-4">
+                        <template v-for="social in appSettings.social_networks" :key="social.network">
+                            <a :href="social.url" target="_blank">
+                                <i
+                                    :class="`text-3xl text-burgundy fa-brands fa-${social.network === 'twitter' ? 'x-twitter' : social.network}`"></i>
+                            </a>
+                        </template>
+                    </div>
                 </div>
+                <figure class="hidden md:inline-block w-[80px] -my-8">
+                    <img class="-mb-[2px]" src="/assets/grecaGuinda.png" alt="" />
+                </figure>
             </div>
-            <figure class="hidden md:inline-block w-[6rem] -my-8">
-                <img class="-mb-[9px]" src="/assets/grecaGuinda.png" alt="" />
-            </figure>
         </div>
     </div>
 </template>
