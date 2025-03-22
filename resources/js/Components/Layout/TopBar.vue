508443bb1a1185b240409849e3598b244d060336
miguelbonifaz <miguelbonifaz126@gmail.com>
2025-03-21 19:08:09 -0500

    removed "español" link

--- resources/js/Components/Layout/TopBar.vue
+++ resources/js/Components/Layout/TopBar.vue
@@ -5,15 +5,14 @@ const appSettings = usePage().props.app_settings
 </script>
 
 <template>
-    <div class="border-b border-burgundy bg-burgundy -mx-4">
-        <div class="flex items-center justify-between px-6 py-2 -mt-8 max-w-7xl mx-auto">
+    <div class="-mx-4 border-b border-burgundy bg-burgundy">
+        <div class="flex items-center justify-between px-6 py-2 mx-auto -mt-8 max-w-7xl">
             <div class="flex items-center">
                 <img class="w-[9rem]" :src="`/storage/${appSettings.mexico_logo}`" alt="Logo Gobierno de México" />
             </div>
             <div class="flex items-center space-x-4 text-white">
                 <a href="#" class="text-sm hover:text-gold">Noticias</a>
                 <a href="#" class="text-sm hover:text-gold">Gobierno</a>
-                <a href="#" class="text-sm hover:text-gold">Español</a>
             </div>
         </div>
     </div>
