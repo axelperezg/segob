7a5022f761472b5d2d565941d99a003db118b9e2
miguelbonifaz <miguelbonifaz126@gmail.com>
2025-03-24 19:40:36 -0500

    wip

--- app/Http/Controllers/MexicoNewsController.php
+++ app/Http/Controllers/MexicoNewsController.php
@@ -11,6 +11,21 @@ class MexicoNewsController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return Inertia::render('MexicoNews');
+        $mainPosts = MexicoNews::query()
+            ->where('is_published', true)
+            ->orderBy('date', 'desc')
+            ->take(3)
+            ->get();
+
+        $posts = MexicoNews::query()
+            ->where('is_published', true)
+            ->orderBy('date', 'desc')
+            ->skip(3)
+            ->paginate(10);
+
+        return Inertia::render('MexicoNews', [
+            'mainPosts' => MexicoNewsResource::collection($mainPosts),
+            'posts' => MexicoNewsResource::collection($posts),
+        ]);
     }
 }
