7d5354b4b28d4161474fd437ffd18ed3091d1746
miguelbonifaz <miguelbonifaz126@gmail.com>
2025-04-14 20:35:52 -0500

    Added favicon for Segob News Website

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/43a8eadd9b.js" crossorigin="anonymous"></script>
+    <x-favicon />
     @routes
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     @inertiaHead
