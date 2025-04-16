a665ebf92add6b8ebaaba2ae14f6a6e616f9e70a
miguelbonifaz <miguelbonifaz126@gmail.com>
2025-04-15 22:03:27 -0500

    Updated favicon links in the component to use asset helper for proper path resolution.

--- resources/views/components/favicon.blade.php
+++ resources/views/components/favicon.blade.php
@@ -1,6 +1,5 @@
-{{-- Favicon Component --}}
-<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-<link rel="shortcut icon" href="/favicon.ico" />
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-<link rel="manifest" href="/site.webmanifest" /> 
+<link rel="icon" type="image/png" href="{{ asset('assets/favicon/favicon-96x96.png') }}" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="{{ asset('assets/favicon/favicon.svg') }}" />
+<link rel="shortcut icon" href="{{ asset('assets/favicon/favicon.ico') }}" />
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-touch-icon.png') }}" />
+<link rel="manifest" href="{{ asset('assets/favicon/site.webmanifest') }}" /> 
