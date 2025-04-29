c4c6b065ed302b64664ae3cd47ce1ba248b62667
miguelbonifaz <miguelbonifaz126@gmail.com>
2025-04-29 06:42:03 -0500

    Added seo field for ActionResource

--- app/Filament/Resources/ActionResource/Pages/CreateAction.php
+++ app/Filament/Resources/ActionResource/Pages/CreateAction.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\ActionResource\Pages;
 
 use App\Filament\Resources\ActionResource;
+use App\Models\Action;
 use Filament\Resources\Pages\CreateRecord;
+use RalphJSmit\Laravel\SEO\Models\SEO;
 
 class CreateAction extends CreateRecord
 {
@@ -12,5 +14,21 @@ class CreateAction extends CreateRecord
     public function getTitle(): string
     {
         return 'Nueva acción';
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function afterCreate(): void
+    {
+        SEO::updateOrCreate([
+            'model_id' => $this->record->id,
+            'model_type' => Action::class,
+        ], [
+            'title' => $this->data['meta_title'],
+            'description' => $this->data['meta_description'],
+        ]);
     }
 }
--- app/Filament/Resources/ActionResource/Pages/EditAction.php
+++ app/Filament/Resources/ActionResource/Pages/EditAction.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\ActionResource\Pages;
 
 use App\Filament\Resources\ActionResource;
+use App\Models\Action;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use RalphJSmit\Laravel\SEO\Models\SEO;
 
 class EditAction extends EditRecord
 {
@@ -15,5 +17,31 @@ class EditAction extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $seo = SEO::where('model_id', $this->record->id)->where('model_type', Action::class)->first();
+        
+        $data['meta_title'] = $seo?->title;
+        $data['meta_description'] = $seo?->description;
+
+        return $data;
+    }
+
+    protected function afterSave(): void
+    {
+        SEO::updateOrCreate([
+            'model_id' => $this->record->id,
+            'model_type' => Action::class,
+        ], [
+            'title' => $this->data['meta_title'],
+            'description' => $this->data['meta_description'],
+        ]);
     }
 }
